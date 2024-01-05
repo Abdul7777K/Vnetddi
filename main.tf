e7244e678c0f5790105f735d92220e39fa39044e
Abdul <Abdul@example.com>
2024-01-05 09:35:50 +0530

    Vnet2

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ resource "azurerm_virtual_network" "my_vnet" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
   location            = var.location
-  resource_group_name = azurerm_resource_group.example
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "my_subnet" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.example
+  resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.my_vnet.name
   address_prefixes     = var.subnet_prefixes
 }
